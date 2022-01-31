095f9ca05594aa26fc40023410177b55650ab973
Evance23 <evancebarracks01@gmail.com>
2022-02-01 00:45:20 +0300

    create admin panel

--- mapics/gallery/models.py
+++ mapics/gallery/models.py
@@ -11,9 +11,9 @@ class Category(models.Model):
     
     
 class Photo(models.Model):
-    category = models.ForeignKey(Category, on_delete= models.Set_Null, null=True)
+    category = models.ForeignKey(Category, on_delete= models.SET_NULL, null=True)
     image = models.ImageField(null=False, blank=False)
-    description = models.CharField(max_length=100, null= False, blank = False)
+    description = models.TextField()
     
     def __str__(self):
         return self.description 
